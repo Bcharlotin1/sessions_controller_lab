a88de29d65c1e04a97717bdf9b6a14eae9286277
bcharlotin1 <biancacharlotin1@gmail.com>
2021-07-29 13:24:41 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+   
+  end
+
+  def create
+    # byebug
+    session[:name] = params[:name]
+
+    if session[:name].blank?
+      redirect_to '/login'
+    else
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    
+  end
+end
